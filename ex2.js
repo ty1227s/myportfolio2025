fc41be63a2ea8d3d9d5479a7cd1adb4eff5d791c
ty1227s <ty_2017_2712@yahoo.co.jp>
2025-05-11 22:31:09 +0900

    5/11分

---
+++ ex2.js
@@ -0,0 +1,15 @@
+// MVを表示しているとき？or一番上にある時、固定表示のヘッダーを表示しない
+function hiddenHeader() {
+
+}
+
+// 一番上にいない時、backtotopボタンを作動させる
+// →hiddenheaderと合体させてもいいか？
+function displayBackToTop() {
+
+}
+
+
+
+
+
