0990cb075d2392e822535d8106708715c7b0a472
ty1227s <ty_2017_2712@yahoo.co.jp>
2025-05-24 17:55:22 +0900

    ～5/24夕方

--- ex1.js
+++ ex1.js
@@ -1,1 +1,34 @@
-// sss
+// // ハンバーガーメニューを開いているときの動作
+// const openBtn = document.querySelector();
+// const menu = document.querySelector();
+
+// openBtn.addEventListener('click', () => {
+//   openBtn.classList.toggle('active');
+//   menu.classList.toggle('active');
+//   document.body.classList('no-scroll');
+// })
+/* ハンバーガーメニューボタン用 wx1.jsに移植する？*/
+    document.addEventListener('DOMContentLoaded', function () {
+      const menuBtn = document.querySelectorAll('.header__menuButton'); /*? */
+      
+      
+      menuBtn.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+          this.classList.toggle('active');
+
+          const menuSp = document.querySelector('.header__menu-sp');
+          if (menuSp) {
+            menuSp.classList.toggle('active');
+          }
+
+        });
+      });
+    });
+
+    // 画面サイズ変更時にメニューを自動で閉じる → 要改善
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 1000) {
+        menuBtn.classList.remove('active');
+        menuSp.classList.remove('active');
+      }
+    });
