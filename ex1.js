6de59afe8f561adf69fb7883920be4aae2717f36
ty1227s <ty_2017_2712@yahoo.co.jp>
2025-05-29 16:38:57 +0900

    ～5/29夕方

--- ex1.js
+++ ex1.js
@@ -8,27 +8,68 @@
 //   document.body.classList('no-scroll');
 // })
 /* ハンバーガーメニューボタン用 wx1.jsに移植する？*/
-    document.addEventListener('DOMContentLoaded', function () {
-      const menuBtn = document.querySelectorAll('.header__menuButton'); /*? */
+document.addEventListener('DOMContentLoaded', function () {
+  const menuBtn = document.querySelectorAll('.header__menuButton'); /*? */
       
       
-      menuBtn.forEach(function (btn) {
-        btn.addEventListener('click', function () {
-          this.classList.toggle('active');
+  menuBtn.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      this.classList.toggle('active');
 
-          const menuSp = document.querySelector('.header__menu-sp');
-          if (menuSp) {
-            menuSp.classList.toggle('active');
-          }
-
-        });
-      });
-    });
-
-    // 画面サイズ変更時にメニューを自動で閉じる → 要改善
-    window.addEventListener('resize', () => {
-      if (window.innerWidth > 1000) {
-        menuBtn.classList.remove('active');
-        menuSp.classList.remove('active');
+      const menuSp = document.querySelector('.header__menu-sp');
+      
+      if (menuSp) {
+        menuSp.classList.toggle('active');
       }
+
     });
+  });
+});
+
+
+// 画面サイズ変更時にメニューを自動で閉じる
+// ※「DOM」はすべて大文字にすること。
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // メニューボタンのクラスを取得
+  const menuBtn = document.querySelector('.header__menuButton');
+      
+  // レスポンシブメニューのクラスを取得
+  const menuSp = document.querySelector('.header__menu-sp');
+      
+  // 画面サイズの変更を検知
+  window.addEventListener('resize', () => {
+        
+    // もし1000pxより広い幅であれば…
+    if (window.innerWidth > 1000) {
+          
+      // ｸﾗｽ名から「.active」を除去→ﾒﾆｭｰﾎﾞﾀﾝ･ﾚｽﾎﾟﾝｼﾌﾞﾒﾆｭｰ非表示化
+      menuBtn.classList.remove('active');
+      menuSp.classList.remove('active');
+    }
+  });
+});
+
+
+//process各要素に番号を付与
+// index = 番号
+document.querySelectorAll('.process__content').forEach((el, index) => {
+  el.setAttribute('data-index', index + 1);
+});
+
+
+// スクロールで到達したら表示させる
+// 'fade-in'の含まれるクラスを取得
+const fadeTargets = document.querySelectorAll('.fade-in');
+
+// 対象クラスすべてに’.active'を付与
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+fadeTargets.forEach(target => observer.observe(target));
--- ex2.js
+++ ex2.js
@@ -35,10 +35,15 @@ $(document).ready(function() {
 //(クラス名に「」を付与する)
 if (window.innerWidth > 768) {
   window.addEventListener('scroll', () => {
+    // headerを取得
     const header = document.querySelector('.header');
+    // headerのロゴを取得
     const logo = document.querySelector('.header__logo-content');
+    // sloganの高さを取得
     const slogan = document.querySelector('.slogan');
     const sloganHeight = slogan.offsetHeight;
+
+    // スクロール位置がsloganの高さを超えたらクラスを追加
     if (window.scrollY > sloganHeight) {
       header.classList.add('top');
       logo.classList.add('top');
@@ -51,7 +56,9 @@ if (window.innerWidth > 768) {
 
 // ページを読み込んでから3秒後にシンボルマークを表示
 window.addEventListener('load', () => {
+  // PC表示時のシンボルマーククラスを取得
   const target1 = document.querySelector('.mv__symbol-img');
+  // SP表示時のシンボルマーククラスを取得
   const target2 = document.querySelector('.mv__symbol-sp-img');
 
   // ページ読み込み後すぐにクラスを追加
