8a51e2604f1cefbb60204b515bb88d934c046649
ty1227s <ty_2017_2712@yahoo.co.jp>
2025-07-25 15:26:50 +0900

    7/25

--- guide.js
+++ guide.js
@@ -1,4 +1,4 @@
-//works各要素に番号を付与
+//works各要素に番号を付与・ファイルパスを生成
 // index = 番号
 const items = document.querySelectorAll('.works__unit');
 const total = items.length;
@@ -7,6 +7,14 @@ items.forEach((item, index) => {
   const number = total - index;
   // ダブルクォートで囲む
   const quoteValue = `"${number}"`;
+  // ファイルパスを生成
+  const filePass = `img/pc${number}.jpg`;
+  // const grandchild = document.querySelector('a .works__unit-img');
+  // console.log(filePass);
+  console.log(quoteValue);
+  // grandchild.style.backgroundImage = `url(${filePass})`;
+  // console.log(grandchild.style.backgroundImage);
+
   // CSS変数を定義
   item.style.setProperty('--works-number', quoteValue);
 });
